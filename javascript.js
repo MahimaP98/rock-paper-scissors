e383c2a30b0bf8d9ddc21148f1186b993b529ee2
Mahima P <mahimaprajapati98@gmail.com>
2026-02-11 18:10:53 +0530

    Assignment rock-paper-scissor UI completed

--- javascript.js
+++ javascript.js
@@ -1,80 +1,125 @@
-//step2: logic to get comupter choice
-function getComputerChoice(){
-    const randomChoice = ["rock", "paper", "scissor"];
-    const randomChoiceIndex = Math.floor(Math.random() * randomChoice.length);
-    return randomChoice[randomChoiceIndex];
+let userScore = 0;
+let computerScore = 0;
+const WINNING_SCORE = 5;
+
+const choices = document.querySelectorAll('.choice');
+const msg = document.querySelector('#msg');
+const userScorePara = document.querySelector('#user-score');
+const computerScorePara = document.querySelector('#computer-score'); 
+let visible = document.querySelector(".msg-container");
+let visibleRestart = document.querySelector(".restart-container");
+
+const generateComputerChoice = () => {
+     const options = ["rock", "paper", "scissor"];
+     const randIdx = Math.floor(Math.random() * 3);
+     return options[randIdx];
 }
 
-//step3: logic to get human choice
-function getHumanChoice(humanChoiceInput){
-    return humanChoiceInput.toLowerCase();
+const drawGame = () => {
+    msg.innerText ="Game was Draw. Play again!";
+    msg.style.backgroundColor = "#081b31";
 }
 
-
-//step5: logic to play single round
-function playRound(humanSelection, computerSelection){
-
-     if((humanSelection === "paper") && (computerSelection === "paper") || 
-    (humanSelection === "rock") && (computerSelection === "rock") || 
-    (humanSelection === "scissor") && (computerSelection === "scissor")){
-        // console.log("It's a tie!");
-        return "Tie";
-    }
-
-    if((humanSelection === "rock") && (computerSelection === "scissor")){
-        // console.log("Computer lose, Rock beats Scissor!");
-        return "Human";
-    }else if((humanSelection === "paper") && (computerSelection === "rock")){
-        // console.log("Computer lose, Paper beats Rock!");
-        return "Human";
-    }else if((humanSelection === "scissor") && (computerSelection === "paper")){
-        // console.log("Computer lose, Scissor beats Paper!");
-        return "Human";
-    }else{
-        // console.log(`You lose, ${computerSelection} beats ${humanSelection}`);
-        return "Computer";
-    }
-}
-
-   let humanScore = 0;
-    let computerScore = 0;
-    let tieScore = 0;
-
-function playGame(){
-    let humanChoiceInput = prompt("Enter rock/paper/scissor: ",'');
-
-    const humanSelection = getHumanChoice(humanChoiceInput);
-    const computerSelection = getComputerChoice();
-    // console.log(humanSelection, computerSelection);
-
-    const roundWinner = playRound(humanSelection, computerSelection);
-    // console.log(roundWinner);
-
-       //step4: declare players score variables 
-    if(roundWinner === "Computer"){
+const showWinner = (userWin, userChoice, computerChoice) => {
+    if(userWin){
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText = `You win! Your ${userChoice} beats ${computerChoice}`;
+        msg.style.backgroundColor = "green";
+    } else {
         computerScore++;
-    }else if(roundWinner === "Human"){
-        humanScore++;
-    }else if(roundWinner === "Tie"){
-        tieScore++;
+        computerScorePara.innerText = computerScore;
+        msg.innerText = `You lose! ${computerChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor = "red";
+  
+    }
+
+    // Check final winner
+    if (userScore === WINNING_SCORE) {
+        msg.innerText = "🎉 You won the game! Click Restart to play again.";
+        msg.style.backgroundColor = "darkgreen";
+        disableChoices();
+        showRestartButton();
+    } 
+    else if (computerScore === WINNING_SCORE) {
+        msg.innerText = "💻 Computer won the game! Click Restart to try again.";
+        msg.style.backgroundColor = "darkred";
+        disableChoices();
+        showRestartButton();
     }
 
 }
 
-for(let i=1; i<=5; i++){
-    playGame();
-}
+const disableChoices = () => {
+    choices.forEach(choice => {
+        choice.style.pointerEvents = "none";
+    });
+};
 
- if(computerScore > humanScore){
-        console.log(`Computer Wins with ${computerScore} times`);
-         console.log(`Round got tie ${tieScore}, and Human won with ${humanScore} times`);
-    }else if(humanScore > computerScore){
-        console.log(`Human Wins with ${humanScore} times`);
-         console.log(`Round got tie ${tieScore}, and Computer won with ${computerScore} times`);
+const enableChoices = () => {
+    choices.forEach(choice => {
+        choice.style.pointerEvents = "auto";
+    });
+};
+
+const playGame = (userChoice) => {
+    // console.log("playGame function is called with userChoice:", userChoice);
+    const computerChoice = generateComputerChoice();
+    // console.log("Computer choice: ", computerChoice);
+
+    if(userChoice === computerChoice){
+        drawGame();
+    } else{
+        let userWin = true;
+        if(userChoice === "rock" ){
+            //scissors, paper
+            userWin = computerChoice === "paper" ? false : true;
+        } else if(userChoice === "paper"){
+            //rock, scissor
+            userWin = computerChoice === "scissor" ? false : true;
+        }   else{ 
+            //rock, paper
+            userWin = computerChoice === "rock" ? false : true;
+        }
+        showWinner(userWin, userChoice, computerChoice);    
     }
 
+}
 
+const restartGame = () => { 
+    userScore = 0;
+    computerScore = 0;
+    userScorePara.innerText = userScore;
+    computerScorePara.innerText = computerScore;
+    msg.innerText = "Game restarted. Play again!";
+    msg.style.backgroundColor = "#081b31";
+};
 
+const showRestartButton = () => {
+  let restartBtn = document.querySelector("#restart-btn");
+
+    if (!restartBtn) {
+        restartBtn = document.createElement("button");
+        restartBtn.id = "restart-btn";
+        restartBtn.innerText = "Restart Game";
+        restartBtn.style.marginTop = "20px";
+
+        restartBtn.addEventListener("click", () => {
+            restartGame();
+            enableChoices();
+            restartBtn.remove();
+        });
+
+        msg.parentElement.appendChild(restartBtn);
+    }
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener('click', () => {
+            const userChoice = choice.getAttribute("id");
+            playGame(userChoice);
+    })
+})
 
 
 
